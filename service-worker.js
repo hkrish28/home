48b03d3c3fe073377d3dbf20de4fd3f7629daccd
Devan K S <kdevan1@ford.com>
2022-01-30 22:19:07 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.8fd4182c6608e03158ae18e98b0ac785.js"
 );
 
 self.addEventListener('message', (event) => {
